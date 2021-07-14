8bbe24bc72d7ad7f12eae40bc4f0c57f4c1825d3
Mateus Campos <mateuscamposantos@gmail.com>
2021-07-14 17:07:34 -0300

    Corrigindo erros
    Adicionando dependência sucrase, para corrigir problemas com importação. Concentrando a aplicação no arquivo server.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,3 +1,13 @@
+const User = require('../models/user')
+
 export default class AuthController {
-  async user(req, res) {}
+  async register(req, res) {
+    try {
+      const user = await User.create(req.body)
+
+      return res.send({ user })
+    } catch (err) {
+      return res.status(400).send({ error: 'Resgistration failed' })
+    }
+  }
 }
--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
-const app = require('../config/express')()
-
+import express from 'express'
+import routes from './routes/users'
+const bodyParser = require('body-parser')
+const app = express()
 const port = 8080
 
-app.listen(port, () =>{
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(routes)
+
+app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
 })
-
