4e7d1a9e7222fd4b249ad3ce7ee29bdf36dd981c
Mateus Campos <mateuscamposantos@gmail.com>
2021-07-14 15:38:36 -0300

    Config server
    Configurando o server

--- config/express.js
+++ config/express.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser')
 module.exports = () => {
   const app = express()
 
-  app.set('port')
-
   app.use(bodyParser.urlencoded({extended: true}))
   app.use(bodyParser.json())
 
