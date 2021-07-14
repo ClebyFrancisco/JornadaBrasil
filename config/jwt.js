13e53e9a5d8e36c53da3ce346d32196d06bba645
Mateus Campos <mateuscamposantos@gmail.com>
2021-07-14 14:40:04 -0300

    Reset

--- config/jwt.js
+++
@@ -1,5 +0,0 @@
-jwtConfig = {
-  secret: 'asdsdff645sd654fsad54f'
-}
-
-module.exports = jwtConfig
--- src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('../config/express')()
-const port = app.get('port')
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`)
-})
