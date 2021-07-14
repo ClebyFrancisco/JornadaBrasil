53ea2e42ffed2d846ea4202d452788a59a6d4458
Mateus Campos <mateuscamposantos@gmail.com>
2021-07-14 13:42:05 -0300

    Alteração
    Alteração de coluna login para username

--- src/database/migrations/20210714144205_create_tab_login.js
+++ src/database/migrations/20210714144205_create_tab_login.js
@@ -3,7 +3,7 @@ exports.up = function (knex) {
     .dropTableIfExists('logins')
     .createTable('logins', table => {
       table.increments('idLogin', 'INT').notNullable().primary()
-      table.specificType('login', 'VARCHAR(18)').notNullable().unique()
+      table.specificType('username', 'VARCHAR(18)').notNullable().unique()
       table.specificType('password', 'VARCHAR(12)').notNullable()
     })
 }
