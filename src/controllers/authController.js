f646fbc4c06b39cd54f3f0e07997bf0c807017c2
Mateus Campos <mateuscamposantos@gmail.com>
2021-07-14 17:39:16 -0300

    Melhorando o tratamento de erro
    Melhorando o tratamento de erro para o front e não retornando a senha para o usuário

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,8 +2,15 @@ const User = require('../models/user')
 
 export default class AuthController {
   async register(req, res) {
+    const { email } = req.body
+
     try {
+      if (await User.findOne({ email })) {
+        return res.status(400).send({ error: 'User already existis' })
+      }
       const user = await User.create(req.body)
+
+      user.password = undefined
 
       return res.send({ user })
     } catch (err) {
