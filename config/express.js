1909011a220f195011562172c8fc77ba9fb42756
Mateus Campos <mateuscamposantos@gmail.com>
2021-07-14 16:24:51 -0300

    Conexão MongoDB
    Criando a conexão com o MongoDB

--- config/express.js
+++ config/express.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+import routes from '../src/routes/users'
 
 module.exports = () => {
   const app = express()
+
+  app.use(routes)
 
   app.use(bodyParser.urlencoded({extended: false}))
   app.use(bodyParser.json())
