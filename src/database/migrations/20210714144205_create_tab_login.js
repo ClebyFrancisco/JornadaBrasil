b502dce8606460afcef4769b0a106095f23741ce
Mateus Campos <mateuscamposantos@gmail.com>
2021-07-14 14:28:23 -0300

    Update 20210714144205_create_tab_login.js

--- src/database/migrations/20210714144205_create_tab_login.js
+++ src/database/migrations/20210714144205_create_tab_login.js
@@ -1,13 +1,11 @@
 exports.up = function (knex) {
-  return knex.schema
-    .dropTableIfExists('logins')
-    .createTable('logins', table => {
-      table.increments('idLogin', 'INT').notNullable().primary()
-      table.specificType('username', 'VARCHAR(18)').notNullable().unique()
-      table.specificType('password', 'VARCHAR(12)').notNullable()
-    })
+  return knex.schema.dropTableIfExists('users').createTable('users', table => {
+    table.increments('idUser', 'INT').notNullable().primary()
+    table.specificType('username', 'VARCHAR(18)').notNullable().unique()
+    table.specificType('password', 'VARCHAR(12)').notNullable()
+  })
 }
 
 exports.down = function (knex) {
-  return knex.schema.dropTable('logins')
+  return knex.schema.dropTable('users')
 }
