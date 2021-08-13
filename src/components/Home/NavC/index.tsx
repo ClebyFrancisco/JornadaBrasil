51a25818f668fb1681030be8448bf3c7708e9dc0
Mateus Campos <mateuscamposantos@gmail.com>
2021-08-12 22:36:40 -0300

    Create NavC

--- src/components/Home/NavC/index.tsx
+++ src/components/Home/NavC/index.tsx
@@ -1,37 +1,60 @@
-import React from "react";
-import  Link from 'next/link';
+import React, {useState} from "react";
+import { TabContent, TabPane, Nav, NavItem, NavLink, Row, Col } from "reactstrap";
+import classnames from 'classnames'
 import styles from './styles.module.scss'
 
-const NavC = () =>{
+const NavC = (props) =>{
+    const [activeTab, setActiveTab] = useState('1');
+
+    const toggle = (tab) => {
+        if(activeTab !== tab) setActiveTab(tab);
+      }
+
     return(
-        <div className={styles.navRegiao}>
-           <ul>
-            <li>
-                <Link href="/conheca/norte">
-                    <a>Historia</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/conheca/nordeste">
-                    <a>Cultura</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/conheca/centro-oeste">
-                    <a>Pontos Turísticos</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/conheca/sudeste">
-                    <a>Hotel</a>
-                </Link>
-            </li>
-            <li>
-                <Link href="/conheca/sul">
-                    <a>Restaurantes</a>
-                </Link>
-            </li>
-        </ul>
+        <div>
+            <Nav tabs className={styles.navConheca}>
+                <NavItem>
+                    <NavLink className={classnames({ active: activeTab === '1' })} onClink={() => { toggle('1')}}>
+                        Historia
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className={classnames({ active: activeTab === '2' })} onClink={() => { toggle('2')}}>
+                        Cultura
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className={classnames({ active: activeTab === '3' })} onClink={() => { toggle('3')}}>
+                        Pontos Turisticos
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className={classnames({ active: activeTab === '4' })} onClink={() => { toggle('4')}}>
+                        Hotel
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink className={classnames({ active: activeTab === '5' })} onClink={() => { toggle('5')}}>
+                        Restaurantes
+                    </NavLink>
+                </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">
+                    <Row>
+                        <Col sm="12">
+                            <h4>Tab 1 Contents</h4>
+                        </Col>
+                    </Row>
+                </TabPane>
+                <TabPane tabId="2">
+                    <Row>
+                        <Col sm="12">
+                            <h4>Tab 2 Contents</h4>
+                        </Col>
+                    </Row>
+                </TabPane>
+            </TabContent>
         </div>
     )
 }
