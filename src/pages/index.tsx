334d991e3b1b75e5ea54b02cb06ec58f4ac8dfd7
Mateus Campos <mateuscamposantos@gmail.com>
2021-08-12 21:31:45 -0300

    Import Nav Conheça +

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import MapaBrasil from '../../public/images/Brasil2.png';
 
 
 import Nav from '../components/Home/Nav';
+import NavC from '../components/Home/NavC'
 import EmAlta from '../components/Roteiros/EmAlta'
 import Image from 'next/image';
 import Banner2 from '../components/Home/Banner2';
@@ -24,7 +25,7 @@ export default function Home() {
       <Image src={MapaBrasil} alt="mapa Brasil"/> 
       </div>
       <Article imagem={imagem2.src} title={"conheça +"}/>
-      <Nav />
+      <NavC />
       <Article imagem={imagem3.src} title={"Roteiros em Alta"}/>
       <div className={styles.emAlta}></div>
       <EmAlta />
