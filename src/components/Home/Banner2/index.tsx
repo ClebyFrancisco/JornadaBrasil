e88c1be1d70f6c89d63102570007f57ee9868012
Cleby Francisco <clebysilva5@gmail.com>
2021-08-12 23:09:14 -0300

    change Banner Home

--- src/components/Home/Banner2/index.tsx
+++ src/components/Home/Banner2/index.tsx
@@ -1,15 +1,37 @@
-import Image from "next/image";
 import React from "react";
-import styles from "./styles.module.scss";
-import imagem1 from '../../../../public/images/ImgBanner/Banner1.jpg';
+import { UncontrolledCarousel, Row, Col } from "reactstrap";
+import styles from "./styles.module.scss"
 
-
-const Banner2 = ()=>{
-    return(
-        <div className={styles.banner}>
-            <Image className={styles.ImagemBanner} src={imagem1} alt="banner" layout="fill" /></div>
-        
-
-    )
-}
+const items = [
+  {
+    src: "https://i.postimg.cc/1z8Nq6qd/Sem-t-tulo.png",
+    altText: "Slide 1",
+    caption: "",
+    header: "",
+    key: "1",
+  },
+  {
+    src: "https://i.postimg.cc/T1Jy6JzK/Sem-t-tulo-2.png",
+    altText: "Slide 2",
+    caption: "",
+    header: "",
+    key: "2",
+  },
+  {
+    src: "https://i.postimg.cc/NG62mQKQ/Sem-t-tulo-1.png",
+    altText: "Slide 3",
+    caption: "",
+    header: "",
+    key: "3",
+  },
+];
+const Banner2 = () => <UncontrolledCarousel items={items}  className={styles.banner}/>;
+/* const Example = () => (
+  <Row>
+    <Col md="8" className="mx-auto">
+      <UncontrolledCarousel items={items} className={styles.} />
+    </Col>
+  </Row>
+);
+ */
 export default Banner2;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Article from '../components/Article'
-import Banner from '../components/Home/Banner'
+/* import Banner from '../components/Home/Banner' */
 import Layout from '../components/layout'
 import styles from '../styles/Home.module.scss';
 import imagem1 from '../../public/images/article/imagem1.jpg';
@@ -12,12 +12,13 @@ import MapaBrasil from '../../public/images/Brasil2.png';
 import Nav from '../components/Home/Nav';
 import EmAlta from '../components/Roteiros/EmAlta'
 import Image from 'next/image';
+import Example from '../components/Home/Banner2';
 import Banner2 from '../components/Home/Banner2';
 
 export default function Home() {
   return (
     <Layout title="Jornada Brasil - Home">
-      <Banner />
+      <Banner2 />
       <Article imagem={imagem1.src} title={"QUAL O DESTINO DOS SEUS SONHOS?"}/>
       <Nav /> 
       <div className={styles.mapaBrasil}>
